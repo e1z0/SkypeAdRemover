dcb7c475321737dddb16bcbf320175b50a470fa9
e1z0 <marcinkeviciusjustinas@gmail.com>
2016-03-01 22:15:54 +0200

    Public release! :-) Links update...

--- SkypeAdRemover/MainForm.cs
+++ SkypeAdRemover/MainForm.cs
@@ -23,7 +23,7 @@ namespace SkypeAdRmover
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start("http://wiki.eofnet.lt/");
-            System.Diagnostics.Process.Start("https://github.com/e1z0");
+            System.Diagnostics.Process.Start("https://github.com/e1z0/SkypeAdRemover");
 
         }
 
